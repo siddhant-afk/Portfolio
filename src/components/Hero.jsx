0beb20261f9ff9126b44ac722e34000a13b608a7
siddhant-afk <siddhantdudhane21@gmail.com>
2024-06-15 18:58:01 +0530

    Add Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ function Hero(){
 
     return(
         <div className="hero">
+            <h5 className="text-green-500">Hi, my name is</h5>
          <h1 className=" font-bold">Siddhant Dudhane</h1>
          <h2 className=" font-bold">Software Developer</h2>
         </div>
